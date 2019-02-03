557f372e15af0e21ba1057130cc0d384b2127110
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2019-02-03 16:55:01 +0700

    Made first sketch of newsblog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { HomepageModule } from './homepage/homepage.module';
+import { NewspageModule } from './newspage/newspage.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomepageModule,
+    NewspageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/newspage/newspage.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewspageComponent } from './newspage.component';
+import { TitleComponent } from './title/title.component';
+import { ArticleComponent } from './article/article.component';
+import { AuthorComponent } from './author/author.component';
+import { RelatedNewsComponent } from './related-news/related-news.component';
+import { DateComponent } from './date/date.component';
+
+@NgModule({
+  declarations: [
+    NewspageComponent,
+    TitleComponent,
+    AuthorComponent,
+    ArticleComponent,
+    RelatedNewsComponent,
+    DateComponent,
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NewspageComponent,
+    TitleComponent,
+    AuthorComponent,
+    ArticleComponent,
+    RelatedNewsComponent
+  ]
+})
+export class NewspageModule { }
