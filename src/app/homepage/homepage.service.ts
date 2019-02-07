e89287c9ecd85a6d783f2bd9ee029c0fddd59406
Stan <k.stanyslaw@gmail.com>
2019-02-07 22:44:27 +0700

    Add news card to homepage component

--- src/app/homepage/homepage.service.ts
+++ src/app/homepage/homepage.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
 
+import { News } from "../models/news";
+
 @Injectable()
 export class HomepageService {
+
+  newsList: News[] = [];
 
   constructor() { }
 
   getNewsList() {
-    return 'Success';
+    const testNews = new News(
+      null,
+      'test Title',
+      'test Author',
+      new Date('2019-02-07 02:17:20.000'),
+      'test Article'
+    );
+    this.newsList.push(testNews);
+    return this.newsList;
   }
 }
