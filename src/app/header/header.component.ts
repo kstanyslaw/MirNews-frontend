9b1d507af8f07c925044a1b1c87f3652929021ec
Stan <k.stanyslaw@gmail.com>
2019-02-11 17:39:29 +0700

    Made language select with bootstrap dropdown

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,12 +8,26 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isShow: boolean = false;
+
   constructor(public translate: TranslateService) {
     translate.addLangs(['en', 'ru']);
     translate.setDefaultLang('en');
 
     const browserLang = translate.getBrowserLang();
     translate.use(browserLang.match(/en|ru/) ? browserLang : 'en');
+  }
+
+  getCountryFlag(language: string) {
+    if (language === 'en') {
+      language = 'us';
+    }
+    return ('flag-icon-' + language);
+  }
+
+  useLanguage(language: string) {
+    this.translate.use(language);
+    this.isShow = false;
   }
 
   ngOnInit() {
