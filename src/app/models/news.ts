90b0b1f3320aa8f07aa26d43f7ef962debc32aec
Stan <k.stanyslaw@gmail.com>
2019-02-12 17:14:05 +0700

    Remade news model

--- src/app/models/news.ts
+++ src/app/models/news.ts
@@ -1,10 +1,11 @@
+import { Body } from './single-language-news-body';
+
 export class News {
     constructor(
         public _id: string,
-        public title: string,
         public author: string,
         public date: Date,
-        public article: string,
+        public body: Body[],
         public category?: string[],
     ) {}
 }
