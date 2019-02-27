cf42d49480dac1dd61b675741569bb6177d56a3c
Stan <k.stanyslaw@gmail.com>
2019-02-27 16:26:56 +0700

    Add get news list method in newslist request page

--- src/app/newslist-request-page/newslist-request-page.service.ts
+++ src/app/newslist-request-page/newslist-request-page.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { News } from '../models/news';
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +10,18 @@ export class NewslistRequestPageService {
 
   pages: number[] = [1,2,3,4,5,6,7];
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   getPagesCount() {
     return this.pages;
+  }
+
+  getNewsList() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.httpClient.get<News[]>('http://localhost:3000/news', httpOptions);
   }
 }
