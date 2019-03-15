f015fa677efa0456ae5e7c90286abc7d9ef4acc2
Stan <k.stanyslaw@gmail.com>
2019-03-15 07:14:09 +0700

    Create category model

---
+++ src/app/models/category.ts
@@ -0,0 +1,9 @@
+export class Category {
+    constructor(
+        public en: string,
+        public ru: string,
+        public navColor: string,
+        public pageColor: string,
+        public _id?: string,
+    ) {}
+}
