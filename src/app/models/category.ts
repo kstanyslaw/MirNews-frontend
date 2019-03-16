aa6fb7c22502a526b7144eb3c3ca2929ddb3b2ef
Stan <k.stanyslaw@gmail.com>
2019-03-16 18:06:21 +0700

    Upgrade models

--- src/app/models/category.ts
+++ src/app/models/category.ts
@@ -2,8 +2,9 @@ export class Category {
     constructor(
         public en: string,
         public ru: string,
-        public navColor: string,
-        public pageColor: string,
+        public navColor?: string,
+        public navbar?: number,
+        public pageColor?: string,
         public _id?: string,
     ) {}
 }
