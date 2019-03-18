7d5db98d7b59959061d8afcceeb7b1c2aa3c5b50
Stan <k.stanyslaw@gmail.com>
2019-03-19 01:55:37 +0700

    Fix & comment datePipe

--- src/app/datepipe/date-format.pipe.ts
+++ src/app/datepipe/date-format.pipe.ts
@@ -9,12 +9,29 @@ export class DateFormatPipe implements PipeTransform {
     var date = new Date(dateJson);
     var now = new Date;
     var dateGap = (+now - +date)/(1000*3600);
-    if (dateGap < 4) {
+    if (dateGap < 1) {                                          // If difference between receved time and current time les than one hour
+      dateGap = dateGap * 60;                                   // Convert to minutes
+      if (dateGap < 1) {                                        // If difference between receved time and current time les than one minute
+        if (language === 'en') {
+          return (' just now')
+        }
+        if (language === 'ru') {
+          return (' только что')
+        }
+      }
       if (language === 'en') {
-        return ('Last' + dateGap + 'hours')
+        return (' ' + dateGap.toFixed(0) + ' minutes ago')
       }
       if (language === 'ru') {
-        return ('Last' + dateGap + 'hours')
+        return (' ' + dateGap.toFixed(0) + ' минуты назад')
+      }
+    }
+    if (dateGap < 4) {                                          // If difference between receved time and current time les than 4 hours
+      if (language === 'en') {
+        return (' ' + dateGap.toFixed(0) + ' hours ago')
+      }
+      if (language === 'ru') {
+        return (' ' + dateGap.toFixed(0) + ' часа назад')
       }
     } else {
       if (language === 'en') {
