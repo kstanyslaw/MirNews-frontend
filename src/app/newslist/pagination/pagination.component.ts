1ec30458296e190db57fc6340b97b2cd2d1dc7e0
Stan <k.stanyslaw@gmail.com>
2019-03-20 18:14:02 +0700

    Workin' on pagination

--- src/app/newslist/pagination/pagination.component.ts
+++ src/app/newslist/pagination/pagination.component.ts
@@ -11,7 +11,7 @@ export class PaginationComponent implements OnInit {
 
   @Input() pages: number;
 
-  pagesArray: number[];
+  pagesArray: any[];
 
   currentPage: number;
 
@@ -20,9 +20,34 @@ export class PaginationComponent implements OnInit {
   getPagesArray() {
     this.pagesArray = [];
     if (this.pages) {
-      for (let i = 0; i < this.pages; i++) {
-        this.pagesArray[i] = i + 1;
-      }
+      if (this.pages <= 7) {
+        for (let i = 0; i < this.pages; i++) {
+          this.pagesArray[i] = i + 1;
+        }
+      } else {
+        if((this.currentPage - 1) < 3) {
+          this.pagesArray[0] = 1;
+          this.pagesArray[1] = 2;
+          this.pagesArray[2] = 3;
+          this.pagesArray.push('...');
+          this.pagesArray.push(this.pages);
+        } else if((this.pages - this.currentPage) < 3) {
+          this.pagesArray[0] = 1;
+          this.pagesArray[1] = '...';
+          this.pagesArray[2] = this.pages - 3;
+          this.pagesArray[3] = this.pages - 2;
+          this.pagesArray[4] = this.pages - 1;
+          this.pagesArray[5] = this.pages;
+        } else {
+          this.pagesArray[0] = 1;
+          this.pagesArray[1] = '...';
+          this.pagesArray[2] = this.currentPage - 1;
+          this.pagesArray[3] = this.currentPage;
+          this.pagesArray[4] = this.currentPage + 1;
+          this.pagesArray[5] = '...';
+          this.pagesArray[6] = this.pages;
+        }
+      }      
     }
     return this.pagesArray;
   }
