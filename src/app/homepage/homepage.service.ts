ab65076b80fac51feca7f33b2c2b838972551740
Stan <k.stanyslaw@gmail.com>
2019-03-22 04:27:21 +0700

    Try to create jumbotron carousel

--- src/app/homepage/homepage.service.ts
+++ src/app/homepage/homepage.service.ts
@@ -18,4 +18,12 @@ export class HomepageService {
     }
     return this.httpClient.get<News[]>('http://localhost:3000/news', httpOptions);
   }
+
+  getImportantNews() {
+    return [
+      new News('5c8b7466b4cd582324342d2b', 'Natalia Sizova', '10.03.2019, 14:00', {en: 'Russians protest against government’s censorship', ru: 'Россияне против ограничений интернета'}, null, {en: 'Thousands of people got together to one of the biggest protests in the Russian in Moscow last weekends against Bill’s Internet controls.', ru: 'Собравшиеся скандировали лозунги: «Руки прочь от интернета» и «Великая Россия - свободный интернет».', img: "https://img.znak.com/285958.jpg"}),
+      new News('5c8fd57bea7d6f6c1e9ee186', 'Fakedocs Maker', '10.03.2019, 14:00', {en: 'Random Title', ru: 'Случайный заголовок'}, null, {en: 'Fringilla elementum laoreet cras posuere interdum euismod placerat at tristique sapien posuere consectetur', ru: 'Лик защитник зва вел Пастушьи лес спасенье пел чая Зрю псалтиры дар. Ее гл предвещают на Аз предвожуся ей во се бесплотных Воздаятель ту.', img: "https://loremflickr.com/1280/720/Society?lock=92"}),
+      new News('5c8fd57cea7d6f6c1e9ee3de', 'Fakedocs Maker', '10.03.2019, 14:00', {en: 'Random Title', ru: 'Случайный заголовок'}, null, {en: 'Fringilla elementum laoreet cras posuere interdum euismod placerat at tristique sapien posuere consectetur', ru: 'Лик защитник зва вел Пастушьи лес спасенье пел чая Зрю псалтиры дар. Ее гл предвещают на Аз предвожуся ей во се бесплотных Воздаятель ту.', img: "https://loremflickr.com/1280/720/Busines?lock=92"}),
+    ]
+  }
 }
--- src/app/homepage/jumbotron/jumbotron.component.ts
+++ src/app/homepage/jumbotron/jumbotron.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
+import { HomepageService } from '../homepage.service';
+
+import { News } from 'src/app/models/news';
+
 @Component({
   selector: 'app-jumbotron',
   templateUrl: './jumbotron.component.html',
@@ -8,9 +12,60 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class JumbotronComponent implements OnInit {
 
-  constructor(private translate: TranslateService) { }
+  slides: Slide[] = [];
 
-  ngOnInit() {
+  activeSlide: number = 0;
+
+  constructor(private translate: TranslateService, private homepageService: HomepageService) { }
+
+  getBackgroundImg() {
+    var imageUrl = 'url(' + this.slides[this.activeSlide].news.preview.img + ')';
+    return { 'background-image': imageUrl }
   }
 
+  changeActiveSlide(i: number) {
+    this.slides[this.activeSlide].isActive = false;
+    this.slides[i].isActive = true;
+    this.activeSlide = i;
+  }
+
+  prevSlide(){
+    if (this.activeSlide === 0) {
+      this.slides[this.activeSlide].isActive = false;
+      this.slides[this.slides.length - 1].isActive = true;
+      this.activeSlide = this.slides.length - 1;
+    } else {
+      this.slides[this.activeSlide].isActive = false;
+      this.slides[this.activeSlide - 1].isActive = true;
+      this.activeSlide = this.activeSlide - 1;
+    }
+  }
+
+  nextSlide() {
+    if (this.activeSlide === this.slides.length - 1) {
+      this.slides[this.activeSlide].isActive = false;
+      this.slides[0].isActive = true;
+      this.activeSlide = 0;
+    } else {
+      this.slides[this.activeSlide].isActive = false;
+      this.slides[this.activeSlide + 1].isActive = true;
+      this.activeSlide = this.activeSlide + 1;
+    }
+  }
+
+  ngOnInit() {
+    var news = this.homepageService.getImportantNews();
+    news.forEach(element => {
+      this.slides.push(new Slide(element, false));
+    });
+    this.slides[this.activeSlide].isActive = true;
+    console.log(news);
+  }
+}
+
+class Slide {
+  constructor(
+    public news: News,
+    public isActive: boolean
+  ) {}
 }
